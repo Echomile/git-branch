4b9f1f496e434b7d8354da3cbedc59682ba75bef
Xiao Peng <147246255+Echomile@users.noreply.github.com>
2026-05-17 21:47:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from calculator import add
+
 def main():
-    print("Git branch management experiment")
+    print("Git branch management experiment - B2 version")
+    result = add(10, 5)
+    print("B2 add result:", result)
 
 if __name__ == "__main__":
     main()
